e153cec650cdeca8c89a0a4e283737f31c0f3fb4
Akim Juillerat <akim.juillerat@camptocamp.com>
2023-02-02 15:23:41 +0100

    [FIX] google_calendar: Fix TypeError on union of different sets
    events and recurrences variables are of GoogleEvent type, whereas
    synced_events and synced_recurrences are respectively of calendar.event
    and calendar.recurrence Odoo recordset types.
    If one of the Odoo recordsets is not empty, __or__ function of abc.Set
    will be called and we'll get the following ValueError:
    
    Only GoogleEvent or iterable of dict are supported
    
    Since the return is cast to boolean types anyway, we can avoid this
    TypeError by using `or` operator instead of `|` operator.

--- addons/google_calendar/models/res_users.py
+++ addons/google_calendar/models/res_users.py
@@ -87,7 +87,7 @@ class User(models.Model):
             events = self.env['calendar.event']._get_records_to_sync(full_sync=full_sync)
             (events - synced_events).with_context(send_updates=send_updates)._sync_odoo2google(calendar_service)
 
-        return bool(events | synced_events) or bool(recurrences | synced_recurrences)
+        return bool(events or synced_events or recurrences or synced_recurrences)
 
     @api.model
     def _sync_all_google_calendar(self):
